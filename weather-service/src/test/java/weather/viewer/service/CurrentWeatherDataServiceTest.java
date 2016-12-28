3ca97feb2d51ab47555d44a0eb9e0d3c31407883
James Ashton <james@akanet.co.uk>
2016-12-28 15:50:42 +0000

    Add spring configuration files and wired test file with spring config

--- weather-service/src/test/java/weather/viewer/service/CurrentWeatherDataServiceTest.java
+++ weather-service/src/test/java/weather/viewer/service/CurrentWeatherDataServiceTest.java
@@ -1,7 +1,14 @@
 package weather.viewer.service;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by james on 28/12/2016.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/service-test-context.xml"})
 public class CurrentWeatherDataServiceTest {
 }
