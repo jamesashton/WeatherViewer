560052d4e4150a6c966fd0bf7957b15676f58234
James Ashton <james@akanet.co.uk>
2016-12-31 17:47:52 +0000

    Added SpringBoot web project and added currentWeatherController

---
+++ weather-server/src/main/java/weather/viewer/server/WeatherServerApplication.java
@@ -0,0 +1,12 @@
+package weather.viewer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServerApplication.class, args);
+	}
+}
---
+++ weather-server/src/main/java/weather/viewer/server/controller/CurrentWeatherController.java
@@ -0,0 +1,15 @@
+package weather.viewer.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by james on 31/12/2016.
+ */
+@RestController
+@RequestMapping("/current")
+public class CurrentWeatherController {
+
+}
