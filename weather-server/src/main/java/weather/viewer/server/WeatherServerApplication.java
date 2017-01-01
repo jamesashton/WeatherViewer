c17aec004a438ad8036e54e05b626c4023b327dc
James Ashton <james@akanet.co.uk>
2017-01-01 12:31:45 +0000

    Pulled the CurrentWeatherDataService into the controller and exposed the currentWeatherDataForCity method.

--- weather-server/src/main/java/weather/viewer/server/WeatherServerApplication.java
+++ weather-server/src/main/java/weather/viewer/server/WeatherServerApplication.java
@@ -2,8 +2,10 @@ package weather.viewer.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring/service-context.xml")
 public class WeatherServerApplication {
 
 	public static void main(String[] args) {
--- weather-server/src/main/java/weather/viewer/server/controller/CurrentWeatherController.java
+++ weather-server/src/main/java/weather/viewer/server/controller/CurrentWeatherController.java
@@ -1,9 +1,12 @@
 package weather.viewer.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import weather.viewer.model.CurrentWeatherData;
+import weather.viewer.service.CurrentWeatherDataService;
 
 /**
  * Created by james on 31/12/2016.
@@ -11,5 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/current")
 public class CurrentWeatherController {
+
+    @Autowired
+    CurrentWeatherDataService currentWeatherDataService;
+
+    @RequestMapping(value="/{city}",method = RequestMethod.GET)
+    CurrentWeatherData currentWeatherForCity(@PathVariable("city") String city){
+        CurrentWeatherData currentWeatherData = currentWeatherDataService.currentWeatherDataForCity(city);
+        return currentWeatherData;
+    }
+
 
 }
